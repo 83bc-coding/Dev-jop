9c53b9155dd6f0e2dd71bcf0715b55960b239c4a
83bc-coding <mr.alkhazrji@gmail.com>
2024-08-01 22:24:20 +0300

    ,,

---
+++ src/components/helper/includesText.tsx
@@ -0,0 +1,8 @@
+const includesText = (str1: string, str2: string) => {
+  str1 = str1.trim().toLowerCase();
+  str2 = str2.trim().toLowerCase();
+
+  return str1.includes(str2);
+};
+
+export default includesText;
